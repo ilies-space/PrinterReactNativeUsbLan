f80d58d409cb80af02577ddec29944f2bc488d19
ilies ouldmenouer <63064471+ilyasxdz@users.noreply.github.com>
2020-12-29 16:12:02 +0100

    ticket modal 1

--- App.js
+++ App.js
@@ -22,8 +22,33 @@ export default function App() {
     });
   }
 
+  var date = new Date().getDate();
   function printText(content) {
-    RNUSBPrinter.printBillTextWithCut(content);
+    if (content === 'ticket') {
+      // print ticket modal  :
+
+      //** There are following some formatted TAG that supports by this library.
+
+      // “<M> … </M>”  – for Medium text.
+      // <CM> … </CM> – center align and Medium text.
+      // <C> …</C> – center alignment.
+      // <B> …</B> for bold text.
+
+      RNUSBPrinter.printText('STORE NAME');
+      RNUSBPrinter.printText('OrderID 125666');
+      RNUSBPrinter.printText('ilies ouldmenouer');
+      RNUSBPrinter.printText('---------------------------------------------');
+      RNUSBPrinter.printText('Coca-cola                    150ml 1 x 190 DA');
+      RNUSBPrinter.printText('ifri                                1 x 30 DA');
+      RNUSBPrinter.printText('---------------------------------------------');
+      RNUSBPrinter.printText('Total                                  220 DA');
+      RNUSBPrinter.printText('merci pour vos achats, à bientôt');
+      RNUSBPrinter.printText('---------------------------------------------');
+      RNUSBPrinter.printBillTextWithCut(date);
+    } else {
+      // print wht user write
+      RNUSBPrinter.printBillTextWithCut(content);
+    }
   }
 
   return (
@@ -42,6 +67,10 @@ export default function App() {
       <View style={{alignSelf: 'flex-end', marginRight: 20}}>
         <TouchableOpacity onPress={() => connectPrinter()}>
           <Text style={{color: 'grey'}}>connect</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => printText('ticket')}>
+          <Text style={{color: 'green'}}>Print Ticket</Text>
         </TouchableOpacity>
       </View>
     </View>
