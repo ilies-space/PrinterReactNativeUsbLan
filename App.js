0d35f7d3f937716df1b50e9738c618d406de7a21
ilies ouldmenouer <63064471+ilyasxdz@users.noreply.github.com>
2020-12-30 08:53:42 +0100

    ticket template

--- App.js
+++ App.js
@@ -1,19 +1,23 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Button, Text, TextInput, TouchableOpacity, View} from 'react-native';
 import {RNUSBPrinter} from 'react-native-usb-printer';
 
 export default function App() {
   const [textContent, settextContent] = useState('hello World');
+  const [isConnected, setisConnected] = useState(false);
+  const [devices, setdevices] = useState('');
+
   function connectPrinter() {
     RNUSBPrinter.getUSBDeviceList().then((res) => {
       console.log({res});
-      const devices = res;
+      setdevices(res);
       if (devices && devices.length > 0) {
         RNUSBPrinter.connectPrinter(
           devices[0].vendor_id,
           devices[0].product_id,
         ).then((res_) => {
           alert('succsefully connect to ' + JSON.stringify(devices[0]));
+          setisConnected(true);
         });
       } else {
         alert('no printer found !! ');
@@ -22,12 +26,62 @@ export default function App() {
     });
   }
 
-  function printText(content) {
-    RNUSBPrinter.printBillTextWithCut('<C>HELLO</C>');
+  function printText_(content) {
+    var toPrint = content.toString();
+    // RNUSBPrinter.printText(toPrint);
+    RNUSBPrinter.printBillTextWithCut(toPrint);
+    // RNUSBPrinter.printText(toPrint);
+    // RNUSBPrinter.printText(toPrint);
+    // RNUSBPrinter.printBillTextWithCut('<C>HELLO</C>');
+    // RNUSBPrinter.printText('HOLA');
   }
+
+  const space47 = '------------------------------------------------';
+  const newLine = '                                                ';
+  const itemExmple = 'ProductExmple                               19DA';
+  const totalPrice = 'Total                                      999DA';
+  // const space47 = 'aabbccddffaabbccddffaabbccddffaabbccddffaabbccdd';
+  const storeName = 'NEO POS';
+  var date = Date.now();
+
+  useEffect(() => {
+    console.log('Printing');
+    if (false) {
+      printText_('<CM>' + storeName + '</CM>');
+      printText_(
+        // storeName +
+        // newLine +
+        itemExmple +
+          newLine +
+          itemExmple +
+          newLine +
+          itemExmple +
+          space47 +
+          newLine +
+          totalPrice +
+          newLine +
+          date,
+      );
+      // printText_(
+      //   // storeName +
+      //   newLine + itemExmple,
+      // );
+    }
+    // printText_('HELLO WORLD !');
+  }, []);
 
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Connection statu : </Text>
+      {isConnected ? (
+        <Text style={{color: 'green', fontWeight: 'bold', fontSize: 20}}>
+          connected to:printer id " {devices[0].vendor_id} "
+        </Text>
+      ) : (
+        <Text style={{color: 'red', fontWeight: 'bold', fontSize: 20}}>
+          Not connected
+        </Text>
+      )}
       <TextInput
         style={{borderWidth: 1, paddingHorizontal: 200, paddingVertical: 10}}
         placeholder={'text to be printed ! '}
@@ -35,10 +89,7 @@ export default function App() {
           settextContent(v);
         }}
         onSubmitEditing={() => {
-          for (let index = 0; index < 5; index++) {
-            printText(textContent);
-            console.log('printText(textContent);');
-          }
+          printText_(textContent);
         }}
       />
 
